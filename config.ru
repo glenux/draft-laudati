2a259d9ae20f123d597bd02a9401acbfd006044a
Glenn Y. Rolland <glenux@glenux.net>
2012-12-12 00:59:14 +0100

    Added basic file upload form.

--- config.ru
+++ config.ru
@@ -1,8 +1,11 @@
 
 require 'sinatra'
+
+LAUDATI_ROOT=File.expand_path(File.dirname(__FILE__))
 require File.join(File.dirname(__FILE__), 'lib', 'laudati')
 
 disable :run
+
 
 map '/' do
 	  run Laudati::App
--- lib/laudati.rb
+++ lib/laudati.rb
@@ -3,8 +3,31 @@ require 'sinatra/base'
 
 module Laudati
 	class App < Sinatra::Base
+		set :root, LAUDATI_ROOT
+
 		get '/' do
-			"Well done callaghan"
+			redirect '/upload'
+		end
+
+		get '/upload' do
+			haml :index
+		end
+
+		post '/upload' do
+			unless params[:zipfile] &&
+				(tmpfile = params[:zipfile][:tempfile]) &&
+				(name = params[:zipfile][:filename])
+				@error = "No file selected"
+				return haml :index
+			end
+			STDERR.puts "Uploading file, original name #{name.inspect}"
+			#FIXME: call script to process file & output picture
+			#
+			#while blk = tmpfile.read(65536)
+			#	# here you would write it to its final location
+			#	STDERR.puts blk.inspect
+			#end
+			"Upload complete"
 		end
 	end
 end
